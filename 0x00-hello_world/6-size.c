bb6c000695eac8a431fffaec8cf3f73d6d4d3e35
ThibautDegbetchi <thibautdegbetchi@gmail.com>
2022-10-13 15:05:45 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,7 @@
  */
 int main(void)
 {
-	pintf("Size of a char:" sizeof(char) byte(s)"\nSize of an int:" sizeof(int) byte(s)"\nSize of a long int:" sizeof(long int) byte(s)"\nSize of a long long int:" sizeof(long long int) byte(s)"\nSize of a float:" sizeof(float) byte(s));
+	printf("Size of a char:" sizeof(char) byte(s)"\nSize of an int:" sizeof(
+int) byte(s)"\nSize of a long int:" sizeof(long) byte(s)"\nSize of a long long int:" sizeof(long long) byte(s)"\nSize of a float:" sizeof(float) byte(s));
 	return (0);
 }
