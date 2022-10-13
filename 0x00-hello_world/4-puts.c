99a101738b30e24adf95f477ae3ba5d6a948ebb9
ThibautDegbetchi <thibautdegbetchi@gmail.com>
2022-10-13 14:31:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 int main(void){
-	char ok[]="\"Programming is like building a multilingual puzzle";
-	puts(ok);
-	return 0;
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
