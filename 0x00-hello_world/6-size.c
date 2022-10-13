d9912cfb80b222b739cab333dcfb9b17d33785ae
ThibautDegbetchi <thibautdegbetchi@gmail.com>
2022-10-13 15:01:04 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main for size of variables
+ * Return: 0
+ */
+int main(void)
+{
+	pintf("Size of a char:" sizeof(char) byte(s)"\nSize of an int:" sizeof(int) byte(s)"\nSize of a long int:" sizeof(long int) byte(s)"\nSize of a long long int:" sizeof(long long int) byte(s)"\nSize of a float:" sizeof(float) byte(s));
+	return (0);
+}
