42dc85a96648031ed1686ff7e60f04441f0f1a5a
ThibautDegbetchi <113916447+ThibautDegbetchi@users.noreply.github.com>
2022-10-17 23:03:56 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 	int digit1, digit2;
-	
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
@@ -21,7 +20,7 @@ int main(void)
 			putchar(',');
 			putchar(' ');
 		}
-  	}
+	}
 	putchar('\n');
 	return (0);
 }
